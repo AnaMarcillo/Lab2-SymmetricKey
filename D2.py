e5f3f7350070ea2adaa92f0dfa44a42739d55154
AnaMarcillo <138828744+AnaMarcillo@users.noreply.github.com>
2023-09-09 21:43:45 -0500

    Create D2.py

---
+++ D2.py
@@ -0,0 +1,18 @@
+from Crypto.Cipher import DES
+import hashlib
+import binascii
+import Padding
+
+def des_encrypt(plaintext, password):
+    key = hashlib.sha256(password.encode()).digest()[:8]
+    plaintext = Padding.appendPadding(plaintext, blocksize=Padding.DES_blocksize, mode='CMS')
+    encobj = DES.new(key, DES.MODE_ECB)
+    ciphertext = encobj.encrypt(plaintext.encode())
+    return binascii.hexlify(bytearray(ciphertext)).decode()
+
+val = 'hello'
+password = 'hello123'
+
+# Encrypt
+encrypted_text = des_encrypt(val, password)
+print("Cipher (ECB):", encrypted_text)
